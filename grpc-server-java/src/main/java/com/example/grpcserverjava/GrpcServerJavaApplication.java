51e464bc62096bf7f7bd3f3308e15e5567556aaa
dunkshot <shotdunkshot@gmail.com>
2022-08-30 13:31:31 +0900

    [spring-grpc] grpc server for java

---
+++ grpc-server-java/src/main/java/com/example/grpcserverjava/GrpcServerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpcserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcServerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcServerJavaApplication.class, args);
+	}
+
+}
---
+++ grpc-server-java/src/main/java/com/example/grpcserverjava/GrpcServerService.java
@@ -0,0 +1,22 @@
+package com.example.grpcserverjava;
+
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.chb.examples.lib.HelloReply;
+import org.chb.examples.lib.HelloRequest;
+import org.chb.examples.lib.SimpleGrpc;
+
+
+@GrpcService
+public class GrpcServerService extends SimpleGrpc.SimpleImplBase{
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder()
+                .setMessage("Hello ==> " + request.getName())
+                .build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+
+}
