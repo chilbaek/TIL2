81f1de78a873daacebd79d3cf693e8812336f9b3
dunkshot <shotdunkshot@gamil.com>
2022-09-09 20:43:30 +0900

    #6.4 Clean up

--- frontend/react-for-beginners/src/App.js
+++ frontend/react-for-beginners/src/App.js
@@ -1,36 +1,27 @@
 import {useEffect, useState} from "react";
 
+function Hello(){
+    function byeFn() {
+        console.log("bye :(")
+    }
+    function hiFn(){
+        console.log("created :)");
+        return byeFn;
+    }
+    useEffect(hiFn, []);
+    return <h1>Hello</h1>;
+}
+
 function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("")
-    const onClick = () => setValue((prev) => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-    useEffect(() => {
-        console.log("I run all the time");
-    }, []);
-    useEffect(() => {
-        // if(keyword !== "" && keyword.length > 5) {
-            console.log("I run when 'keyword' changes.", keyword);
-        // }
-    }, [keyword]);
-   useEffect(() => {
-       console.log("I run when 'counter' changes.", counter);
-    }, [counter]);
-  useEffect(() => {
-       console.log("I run when 'counter' & 'keyword' changes.", counter, keyword);
-    }, [counter, keyword]);
-  return (
-    <div>
-        <input
-            value={keyword}
-            onChange={onChange}
-            type="text"
-            placeholder="search here ..."
-        />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me </button>
-    </div>
-  );
+    const [showing, setShwoing] = useState(false);
+    const onClick = () => setShwoing((prev) => !prev)
+
+    return (
+        <div>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+        </div>
+    )
 }
 
 export default App;
